19b84c6c943e1d4318b4015ea796295e420c2d18
ualjam <cococa1027@daou.co.kr>
2025-01-15 13:18:49 +0900

    Add files via upload

---
+++ [실습][DB]250115-실습5번.sql.sql
@@ -0,0 +1,7 @@
+select name, email from customers order by name, email desc;
+
+select account_id, balance from accounts order by balance desc, account_id;
+
+select amount, transaction_type from transactions order by transaction_type, amount desc;
+
+select amount, status from loans order by status, amount desc;
